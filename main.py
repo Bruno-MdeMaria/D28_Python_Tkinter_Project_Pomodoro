63cd8ddfa9d04c1ccd21c77c54336f6ce5fbdece
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-13 16:40:55 -0300

    Update main.py
    repetições do modo pomodoro:
    25+5+25+5+25+5+25+20

--- main.py
+++ main.py
@@ -9,12 +9,25 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+repets = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- MECANISMO DE TEMPO ------------------------------- # 
 def iniciar_timer():
-    contagem_regresiva(contagem= 5 * 60)
+    global repets
+    repets +=1
+
+    work_seg = WORK_MIN *60
+    short_break_seg = SHORT_BREAK_MIN * 60
+    long_break_seg = LONG_BREAK_MIN * 60
+    
+    #se a variavel "repets" for a 1°/3°/5°/7°:
+    contagem_regresiva(work_seg)  #Contagem regressiva inicia com o tempo de trabalho = 25 minutos
+    if repets % 8 == 0    # se a repetição dividida por 8 for igual a resto 0(ou seja na outava repeticao):
+        contagem_regresiva(long_break_seg) #o relógio deverá 
+
+
 
 
 
@@ -23,7 +36,9 @@ def iniciar_timer():
 def contagem_regresiva(contagem):
     contagem_minutos = math.floor(contagem / 60)
     contagem_seg = contagem % 60
-    canvas.itemconfig(tempo_text, text=f"{contagem_minutos}: {contagem_seg}")
+    if contagem_seg < 10:
+        contagem_seg = f"0{contagem_seg}"  #para deixar da forma os segundos 5:00 e 0:09, 0:08.
+    canvas.itemconfig(tempo_text, text=f"{contagem_minutos}:{contagem_seg}")
     if contagem >0:
         window.after(1000, contagem_regresiva, contagem-1)  #a janela depois(after) de 1000 milesegundos(1seg) deve abrir a função 
 
@@ -42,9 +57,9 @@ titulo_label.grid(column=1, row=0)
 
 #IMAGEM CENTRAL DO TOMATE E RELOGIO:
 canvas = Canvas(width=200, height=224, bg=GREEN, highlightthickness=0) #cria uma tela para colocar uma imagem sobreposta. Bg é a cor do fundo da imagem e highligh...é o tamanho da borda(nesse caso sem borda).
-tomato_png = PhotoImage(file="tomato.png")  #importa a imagemdo ficheiro. O destino pode ser direto ou relativo
+tomato_png = PhotoImage(file="tomato.png")  #importa a imagem do ficheiro. O destino pode ser direto ou relativo
 canvas.create_image(100, 112,image = tomato_png)  #cria a imagem e coloca no meio da tela... o eixo x é metade do valor e y também. Função imagem chama a variavel que recebeu o arquivo da imagem
-tempo_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+tempo_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")) #o relogio zerado é uma str informado no "text= 00:00"
 canvas.grid(column=1, row=1)
 
 
