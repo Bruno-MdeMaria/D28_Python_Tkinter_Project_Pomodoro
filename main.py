b209615a354c097088cbf32928f5a8d9b59dcaed
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-13 08:12:58 -0300

    Update main.py
    botoes iniciar , reset e etiqueta de checagem

--- main.py
+++ main.py
@@ -20,12 +20,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, background=GREEN)
 
+#TÍTULO
+titulo_label = Label(text="Timer", foreground= RED, bg=GREEN, font=(FONT_NAME, 40, "bold"))
+titulo_label.grid(column=1, row=0)
+
+#IMAGEM CENTRAL DO TOMATE E RELOGIO:
 canvas = Canvas(width=200, height=224, bg=GREEN, highlightthickness=0) #cria uma tela para colocar uma imagem sobreposta. Bg é a cor do fundo da imagem e highligh...é o tamanho da borda(nesse caso sem borda).
 tomato_png = PhotoImage(file="tomato.png")  #importa a imagemdo ficheiro. O destino pode ser direto ou relativo
 canvas.create_image(100, 112,image = tomato_png)  #cria a imagem e coloca no meio da tela... o eixo x é metade do valor e y também. Função imagem chama a variavel que recebeu o arquivo da imagem
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+#BOTAO INICIAR e RESET:
+button_start = Button(text="Start", highlightthickness=0)
+button_start.grid(column=0, row=2)
 
+button_reset = Button(text="Reset", highlightthickness=0)
+button_reset.grid(column=2,row=2)
 
+#MARCA DE CHACAGEM:
+check_marks = Label(text=" ✔ " , fg="#379237", bg=GREEN)
+check_marks.grid(column=1, row=3)
 window.mainloop()
