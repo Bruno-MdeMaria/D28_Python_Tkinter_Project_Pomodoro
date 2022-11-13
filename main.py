ceb4d376b87590bf2126fb22e47bdce050d6107f
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-13 09:21:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math  #para função matemática do timer
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,16 +12,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- MECANISMO DE TEMPO ------------------------------- # 
+def iniciar_timer():
+    contagem_regresiva(contagem= 5 * 60)
+
+
+
 
 # ---------------------------- MECANISMO DE CONTAGEM DECRECENTE------------------------------- # 
 def contagem_regresiva(contagem):
-    canvas.itemconfig(tempo_text )
+    contagem_minutos = math.floor(contagem / 60)
+    contagem_seg = contagem % 60
+    canvas.itemconfig(tempo_text, text=f"{contagem_minutos}: {contagem_seg}")
     if contagem >0:
         window.after(1000, contagem_regresiva, contagem-1)  #a janela depois(after) de 1000 milesegundos(1seg) deve abrir a função 
-
-
-
 
 
 
@@ -29,7 +34,7 @@ def contagem_regresiva(contagem):
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, background=GREEN)
-contagem_regresiva(contagem=5)
+
 
 #TÍTULO
 titulo_label = Label(text="Timer", foreground= RED, bg=GREEN, font=(FONT_NAME, 40, "bold"))
@@ -42,8 +47,9 @@ canvas.create_image(100, 112,image = tomato_png)  #cria a imagem e coloca no mei
 tempo_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
+
 #BOTAO INICIAR e RESET:
-button_start = Button(text="Start", highlightthickness=0)
+button_start = Button(text="Start", highlightthickness=0, command= iniciar_timer)
 button_start.grid(column=0, row=2)
 
 button_reset = Button(text="Reset", highlightthickness=0)
