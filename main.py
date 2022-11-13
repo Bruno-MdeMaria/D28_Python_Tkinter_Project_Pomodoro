b9f27c5aaf5942b3de362fe539b88d2e5988076d
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-13 07:31:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
+window.title("Pomodoro")
+
+canvas = Canvas(width=200, height=224) #cria uma tela para colocar uma imagem sobreposta
+tomato_png = PhotoImage(file="tomato.png")  #importa a imagemdo ficheiro. O destino pode ser direto ou relativo
+canvas.create_image(100, 112,image = tomato_png)  #cria a imagem e coloca no meio da tela... o eixo x é metade do valor e y também. Função imagem chama a variavel que recebeu o arquivo da imagem
+canvas.pack()
+
+
+
+window.mainloop()
